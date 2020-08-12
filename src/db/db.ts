205900950129dde2078ca9df1e02a96ef18be334
Emilio Franco <ejfranco06@gmail.com>
2020-08-11 20:57:42 -0400

    Add PostgreSQL database

---
+++ src/db/db.ts
@@ -0,0 +1,8 @@
+import { Pool } from 'pg';
+
+export const pool: Pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import dotenv from "dotenv";
-import express, {Request, Response} from "express";
-import cors from "cors";
-import helmet from "helmet";
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 dotenv.config();
+import { pool } from './db/db';
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 
@@ -11,10 +12,20 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req: Request, res: Response) => res.send(`I'm a working server`));
+
+//test database
+app.get('/db', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query('SELECT * FROM test_table');
+    const results = { results: result ? result.rows : null };
+    res.status(201).json(results);
+  } catch (err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
-
